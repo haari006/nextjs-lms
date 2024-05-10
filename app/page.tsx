f50a91cb5f99c950c5a3e677ceea365688f85360
haari006 <128443332+haari006@users.noreply.github.com>
2024-05-10 22:42:22 +0800

    Remove unused import and update link element

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { createClient } from "@/utils/supabase/server";
 import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
 import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
 import Header from "@/components/Header";
-import Link from "next/link";
 
 export default async function Index() {
   const canInitSupabaseClient = () => {
@@ -40,14 +39,14 @@ export default async function Index() {
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
           Powered by{" "}
-          <Link
+          <a
             href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
             target="_blank"
             className="font-bold hover:underline"
             rel="noreferrer"
           >
             Supabase
-          </Link>
+          </a>
         </p>
       </footer>
     </div>
