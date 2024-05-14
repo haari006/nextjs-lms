58490ea21c4631b4caf30e6c6157b4993e96309b
haari006 <128443332+haari006@users.noreply.github.com>
2024-05-14 20:54:49 +0800

    Refactor page layout and add LMS logo and tagline

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,40 @@ export default async function Index() {
 
       <div className="flex-1 flex justify-center items-center">
         <div className="max-w-6xl w-full p-3">
-          {user && <Header user={user} />}
+          <div className="flex flex-col items-center bg-gray-900 text-white">
+            <div className="flex gap-20 justify-center items-center">
+              <Image
+                src="/images/headtech.png"
+                width={100}
+                height={100}
+                alt="LMS Logo"
+                className="mt-5"
+              />
+            </div>
+            <h1 className="sr-only">Learning Management System</h1>
+            <p className="text-3xl lg:text-4xl leading-tight mx-auto max-w-xl text-center">
+              Headtech <br />{" "}
+              <span className="font-bold">Learning Management System </span>{" "}
+              <br />
+              <span className="text-blue-400 text-sm leading-2">
+                Empowering education through technology.
+              </span>
+            </p>
+            <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
+            {user ? (
+              <Link href="/dashboard" className="flex justify-center">
+                <button className="flex justify-center bg-white hover:shadow-indigo-500 shadow-md text-gray-800 font-semibold leading-5 py-2 px-4 rounded">
+                  Let's Learn
+                </button>
+              </Link>
+            ) : (
+              <Link href="/login" className="flex justify-center">
+                <button className="flex justify-center bg-white hover:shadow-indigo-500 shadow-md text-gray-800 font-semibold leading-5 py-2 px-4 rounded">
+                  Join Us
+                </button>
+              </Link>
+            )}
+          </div>
         </div>
       </div>
 
